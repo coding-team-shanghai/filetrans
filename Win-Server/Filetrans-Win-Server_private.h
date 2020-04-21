fe2896662df340895b35c822e77fd922245e51a3
EricNTH080103 <eric_ni2008@163.com>
2020-04-21 11:45:33 +0800

    0.0.5

--- Win-Server/Filetrans-Win-Server_private.h
+++ Win-Server/Filetrans-Win-Server_private.h
@@ -5,19 +5,19 @@
 #define FILETRANS-WIN-SERVER_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.9.0.21"
+#define VER_STRING	"0.9.0.30"
 #define VER_MAJOR	0
 #define VER_MINOR	9
 #define VER_RELEASE	0
-#define VER_BUILD	21
+#define VER_BUILD	30
 #define COMPANY_NAME	""
-#define FILE_VERSION	"0.9.0.21"
+#define FILE_VERSION	"0.9.0.30"
 #define FILE_DESCRIPTION	"Filetrans File Transporter Server - Windows"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	""
-#define PRODUCT_VERSION	"0.9.0.21"
+#define PRODUCT_VERSION	"0.9.0.30"
 
 #endif /*FILETRANS-WIN-SERVER_PRIVATE_H*/
